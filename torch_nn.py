e71d8904942a8e368a9d5a0a7387e05434d1cc2a
liaozhi <liaozhi_edo@163.com>
2022-04-25 20:06:41 +0800

    init

---
+++ torch_nn.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*- 
+# @Author  : liaozhi
+# @Date    : 2022/4/25
+# @Contact : liaozhi_edo@163.com
+
+
+"""
+    Torch模型
+"""
+
+# packages
+
---
+++ train_eval_pred.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*- 
+# @Author  : liaozhi
+# @Date    : 2022/4/25
+# @Contact : liaozhi_edo@163.com
+
+
+"""
+    Torch模型训练&评估&预估
+"""
